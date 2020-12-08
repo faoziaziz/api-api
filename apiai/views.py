8486091de11907239e474281256108e3dd47c0b8
Aziz Faozi <admin@labseni.com>
2020-12-08 16:52:38 +0700

    mantap

--- apiai/views.py
+++ apiai/views.py
@@ -1,8 +1,23 @@
+# author : Aziz Amerul Faozi
+# deskripition : This code use for maker
+#
+
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the apiai index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." %question_id)
+
+def results(request, question_id):
+    response = "You're looking at results of question %s."
+    return HttpResponse(response %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on quesition %s." % question_id)
+    
 
 
